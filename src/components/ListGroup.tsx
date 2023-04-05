93b289e37012abc0abca3685ff2b506df792216b
linka <ilincnedel2710@student.lvusd.org>
2023-04-04 21:33:33 -0700

    getmessage

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,28 @@
 import { Fragment } from "react";
 function ListGroup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris',
+
+    ]
+items = []
+
+const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null
+}
   return (
-    <Fragment>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+    <>
+        <h1>List</h1>
+        {getMessage()}
+        <ul className="list-group">    
+          {items.map(item => (
+            <li key={item}>{item} </li>
+        ))}
         </ul>
-    </Fragment>
+    </>
   );
 }
 
