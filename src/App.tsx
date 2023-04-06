cb5166e7440972d86dde7d44fcbb1637fb7597f2
linka <ilincnedel2710@student.lvusd.org>
2023-04-05 20:24:46 -0700

    display recipe from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import ListGroup from "./components/ListGroup"
+import React, { useState, useEffect } from "react";
 
 function App() {
-  let items: string[] = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris',
-  ]
+  let [apiResponse, setApiResponse] = useState("")
+    
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://19o9wqa834.execute-api.us-west-1.amazonaws.com/',
+        {method: 'GET'}
+      )
+      const recipe = await response.text()
+      setApiResponse(recipe)
+    }
+    fetchData()
+  })
+  
+  function handleSubmit(event: any) {
+    event.preventDefault();  
+  }
 
-  return <div><ListGroup items={items}/></div>;
+  return <div><ListGroup items={[apiResponse]}/></div>;
 }
 
 
