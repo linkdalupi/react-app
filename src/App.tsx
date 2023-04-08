886fe0e1fe6ad9d8c8d568a9bc5952ce4134bae7
linka <ilincnedel2710@student.lvusd.org>
2023-04-07 21:00:35 -0700

    make request always use tomato

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        'https://19o9wqa834.execute-api.us-west-1.amazonaws.com/',
+        'https://19o9wqa834.execute-api.us-west-1.amazonaws.com/?ingredient=tomato',
         {method: 'GET'}
       )
       const recipe = await response.text()
