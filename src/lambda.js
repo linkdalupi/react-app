01b51fe95f6993578448022ad5e58363f101c4a9
linka <ilincnedel2710@student.lvusd.org>
2023-04-07 20:57:21 -0700

    make lambda read query string

--- src/lambda.js
+++ src/lambda.js
@@ -1,8 +1,16 @@
 export const handler = async(event) => {
-    const message = [
-        {"recipe": "You take 1 cup of rice and 1 cup of water. You then grate the skin of an orange. You mix these up and boil it. Add 1 spoon of sugar. When done, season with cinammon."},
-        {"recipe": "2nd"}
-    ]
+    
+    let message;
+    if (event.queryStringParameters.ingredient === "tomato") {
+        message = [
+            {"recipe": "You take 1 cup of rice and 1 cup of water. You then grate the skin of an orange. You mix these up and boil it. Add 1 spoon of sugar. When done, season with cinammon."}
+        ]
+    } else {
+        message = [
+            {"recipe": "2nd"}
+        ]
+    }
+    
     var response = {
         statusCode: 200,
         headers: {
