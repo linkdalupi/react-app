b9fbda1bdcff232d0f8b65c6e0a3fbf27fad8eee
linka <ilincnedel2710@student.lvusd.org>
2023-04-08 20:26:27 -0700

    change UI to show more ingredients and change request to send all ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [apiResponse, setApiResponse] = useState("{}")
   const [submitted, setSubmitted] = useState(false)
-  const [ingredient, setIngredient] = useState("");
+  const [ingredientList, setIngredientList] = useState<string[]>([]);
 
   const handleChange = (event: any) => {
-    setIngredient(event.target.id);
+    const newIngredients = [...ingredientList, event.target.id]
+    setIngredientList(newIngredients);
   }
   
-  function handleSubmit(event: any) {
+  function handleSubmit(event: any) {``
     setSubmitted(true)
-    fetchData(ingredient)
+    fetchData(ingredientList)
     event.preventDefault();  
   }
 
-  async function fetchData(ingredient: string) {
+  async function fetchData(ingredientList: string[]) {
+    console.log(ingredientList)
     const response = await fetch(
-      "https://19o9wqa834.execute-api.us-west-1.amazonaws.com/?ingredient="+ingredient,
+      "https://19o9wqa834.execute-api.us-west-1.amazonaws.com/?ingredients=" + 
+      ingredientList.toString(),
       { method: "GET" }
     );
     const recipe = await response.text();
@@ -29,11 +32,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <label> tomato</label>
-          <input type="checkbox" id="tomato" name="tomato" value="tomato" onChange={handleChange}/>
-          <label> flour</label>
-          <input type="checkbox" id="flour" name="flour" value="flour" onChange={handleChange}/>
+          <label> eggs</label>
+          <input type="checkbox" id="eggs" value="eggs" onChange={handleChange}/>
+          <label> cheese</label>
+          <input type="checkbox" id="cheese" value="cheese" onChange={handleChange}/>
+          <label> spinach</label>
+          <input type="checkbox" id="spinach" value="spinach" onChange={handleChange}/>
+          <label> garlic powder</label>
+          <input type="checkbox" id="garlic_powder" value="garlic powder" onChange={handleChange}/>
+          <label> butter</label>
+          <input type="checkbox" id="butter" value="butter" onChange={handleChange}/>
+          <label> beef</label>
+          <input type="checkbox" id="beef" value="beef" onChange={handleChange}/>
+          <label> rice</label>
+          <input type="checkbox" id="rice" value="rice" onChange={handleChange}/>
+          <label> guacamole</label>
+          <input type="checkbox" id="guacamole" value="guacamole" onChange={handleChange}/>
+          <label> pita bread</label>
+          <input type="checkbox" id="pita_bread" value="pita bread" onChange={handleChange}/>
+          <label> lemon</label>
+          <input type="checkbox" id="lemon" value="lemon" onChange={handleChange}/>
+
           <input type="submit" value="Find recipes"/>
+        
         </div>
       </header>
     </div>
