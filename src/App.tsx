166c79919b4ab1d58ff90c696ed70e744e244592
linka <ilincnedel2710@student.lvusd.org>
2023-04-29 16:58:56 -0700

    switch to using drop down with multiple select

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Select from 'react-select'
+
 import ListGroup from "./components/ListGroup"
 import React, { useState, useEffect } from "react";
 import "./App.css"
@@ -7,18 +9,10 @@ function App() {
   const [submitted, setSubmitted] = useState(false)
   const [ingredientList, setIngredientList] = useState<string[]>([]);
 
-  const handleChange = (event: any) => {
-    if (event.target.checked) {
-      const newIngredients = [...ingredientList, event.target.id]
-      setIngredientList(newIngredients);
-    } else {
-      const index = ingredientList.indexOf(event.target.id);
-      if (index > -1) { 
-        ingredientList.splice(index, 1); 
-      }
-    }
+  const handleSelectChange = (event: any) => {
+    setIngredientList(event.map((selection: any) => {return selection.value}))
   }
-  
+    
   function handleSubmit(event: any) {``
     setSubmitted(true)
     fetchData(ingredientList)
@@ -35,65 +29,38 @@ function App() {
     setApiResponse(recipe);
   }
 
+  const options = [
+    { value: 'eggs', label: 'Eggs' },
+    { value: 'cheese', label: 'Cheese' },
+    { value: 'spinach', label: 'Spinach' },
+    { value: 'garlic_powder', label: 'Garlic Powder' },
+    { value: 'butter', label: 'Butter' },
+    { value: 'beef', label: 'Beef' },
+    { value: 'rice', label: 'Rice' },
+    { value: 'guacamole', label: 'Guacamole' },
+    { value: 'pita_bread', label: 'Pita Bread' },
+    { value: 'lemon', label: 'Lemon' },
+    { value: 'peppers', label: 'Peppers' },
+    { value: 'pork', label: 'Pork' },
+    { value: 'tomatoh_sauce', label: 'Tomatoh Sauce' },
+    { value: 'noodles', label: 'Noodles' },
+    { value: 'carrot', label: 'Carrot' },
+    { value: 'onion', label: 'Onion' },
+    { value: 'bread_crumbs', label: 'Bread crumbs' },
+    { value: 'olive_oil', label: 'Olive Oil' },
+    { value: 'flour', label: 'Flour' },
+    { value: 'potatoh', label: 'Potatoh' },
+    { value: 'paprika', label: 'Paprika' },
+    { value: 'pizza_dough', label: 'Pizza Dough' },
+    { value: 'yeast', label: 'Yeast' },
+  ]
+    
   return (<form onSubmit={handleSubmit}>
     <div className="App">
       <header className="App-header">
+        <Select options={options} isMulti={true} onChange={handleSelectChange}/>
         <div>
-          <label className="ingredient">eggs</label>
-          <input type="checkbox" id="eggs" onChange={handleChange}/>
-          <label className="ingredient">cheese</label>
-          <input type="checkbox" id="cheese" onChange={handleChange}/>
-          <label className="ingredient">spinach</label>
-          <input type="checkbox" id="spinach" onChange={handleChange}/>
-          <label className="ingredient">garlic powder</label>
-          <input type="checkbox" id="garlic_powder" onChange={handleChange}/>
-          <label className="ingredient">butter</label>
-          <input type="checkbox" id="butter" onChange={handleChange}/>
-          <label className="ingredient">beef</label>
-          <input type="checkbox" id="beef" onChange={handleChange}/>
-          <label className="ingredient">rice</label>
-          <input type="checkbox" id="rice" onChange={handleChange}/>
-          <label className="ingredient">guacamole</label>
-          <input type="checkbox" id="guacamole" onChange={handleChange}/>
-          <label className="ingredient">pita bread</label>
-          <input type="checkbox" id="pita_bread" onChange={handleChange}/>
-          <label className="ingredient">lemon</label>
-          <input type="checkbox" id="lemon" onChange={handleChange}/>
-          <label className="ingredient">peppers</label>
-          <input type="checkbox" id="peppers" onChange={handleChange}/>
-          <label className="ingredient">pork</label>
-          <input type="checkbox" id="pork" onChange={handleChange}/>
-          <label className="ingredient">tomatoh sauce</label>
-          <input type="checkbox" id="tomatoh_sauce" onChange={handleChange}/>
-          <label className="ingredient">noodles</label>
-          <input type="checkbox" id="noodles" onChange={handleChange}/>
-          <label className="ingredient">chicken</label>
-          <input type="checkbox" id="chicken" onChange={handleChange}/>
-          <label className="ingredient">carrot</label>
-          <input type="checkbox" id="carrot" onChange={handleChange}/>
-          <label className="ingredient">onion</label>
-          <input type="checkbox" id="onion" onChange={handleChange}/>
-          <label className="ingredient">bread crumbs</label>
-          <input type="checkbox" id="bread_crumbs" onChange={handleChange}/>
-          <label className="ingredient">olive oil</label>
-          <input type="checkbox" id="olive_oil" onChange={handleChange}/>
-          <label className="ingredient">flour</label>
-          <input type="checkbox" id="flour" onChange={handleChange}/>
-          <label className="ingredient">milk</label>
-          <input type="checkbox" id="milk" onChange={handleChange}/>
-          <label className="ingredient">potatoh</label>
-          <input type="checkbox" id="potatoh" onChange={handleChange}/>
-          <label className="ingredient">paprika</label>
-          <input type="checkbox" id="paprika" onChange={handleChange}/>
-          <label className="ingredient">pizza dough</label>
-          <input type="checkbox" id="pizza_dough" onChange={handleChange}/>
-          <label className="ingredient">yeast</label>
-          <input type="checkbox" id="yeast" onChange={handleChange}/>
-
-
-
           <input className="ingredient" type="submit" value="Find recipes"/>
-        
         </div>
       </header>
     </div>
